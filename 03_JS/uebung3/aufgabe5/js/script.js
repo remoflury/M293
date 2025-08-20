628a0f27a688e094a7a84df0217e92901e83ee5b
remoflury <remoflury97+web@gmail.com>
2025-08-20 17:52:09 +0200

    fix: wording

--- 03_JS/uebung3/aufgabe5/js/script.js
+++ 03_JS/uebung3/aufgabe5/js/script.js
@@ -1,10 +1,10 @@
 /**
  * AUFGABE 5
  * 
- * Verwenden Sie fetch(), um von data.json die User-Daten zu laden und wandeln
- * Sie die Daten in JSON um.
+ * Verwende fetch(), um von data.json die User-Daten zu laden und wandle
+ *  die Daten in JSON um.
  * 
- * Geben Sie nach erfolgreichem Laden den Namen (name) des ersten Nutzers
+ * Gebe nach erfolgreichem Laden den Namen (name) des ersten Nutzers
  * mit console.log() in der Konsole aus.
  * 
  * Tipp: der Pfad der Datei ist relativ zu dort wo die Funktion ausgeführt wird (index.html)
